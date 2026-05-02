ee0b646016604a9ecbdc1a1f6cfcb0fb659b95aa
Nagendra Ghimire <nagendra.ghi@gmail.com>
2026-05-02 14:59:46 -0400

    put finetune evaluate in folder result_finetune_annotated

--- run_casanovo_finetune_eval.sh
+++ run_casanovo_finetune_eval.sh
@@ -12,7 +12,7 @@ sudo docker run --rm --runtime=nvidia \
     --shm-size=2g --ulimit memlock=-1 \
     -v $(pwd)/data_mgf_annotated:/data_in:ro \
     -v $(pwd)/bin/casanovo:/ckpt:ro \
-    -v $(pwd)/result_finetune/casanovo:/out \
+    -v $(pwd)/result_finetune_annotated/casanovo:/out \
     casanovo casanovo sequence --evaluate \
       --model /ckpt/casanovo_v5_0_0_v5_0_0.ckpt \
       --output_dir /out --output_root Ecoli_EV_2 \
@@ -22,11 +22,11 @@ sudo docker run --rm --runtime=nvidia \
     --shm-size=2g --ulimit memlock=-1 \
     -v $(pwd)/data_mgf_annotated:/data_in:ro \
     -v $(pwd)/model_finetune/casanovo:/ckpt:ro \
-    -v $(pwd)/result_finetune/casanovo:/out \
+    -v $(pwd)/result_finetune_annotated/casanovo:/out \
     casanovo casanovo sequence --evaluate \
       --model "/ckpt/${CKPT}" \
       --output_dir /out --output_root Ecoli_EV_2.finetuned \
       /data_in/ecoli/Ecoli_EV_2.casanovo.annotated.mgf
 
 # Files written by the container are root-owned on the host. Restore ownership.
-sudo chown -R "$(id -u):$(id -g)" result_finetune
+sudo chown -R "$(id -u):$(id -g)" result_finetune_annotated
